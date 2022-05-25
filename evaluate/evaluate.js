e01fa09bfb2cd40ca8dcc813af7bda76f1989969
Lucas Santana <lucasg4x@gmail.com>
2022-05-25 11:45:50 -0300

    Evaluate Dataset

---
+++ evaluate/evaluate.js
@@ -0,0 +1,141 @@
+const tf = require("@tensorflow/tfjs-node");
+const fs = require("fs");
+const path = require("path");
+
+const TRAIN_FOLDER = "../dataset/train/";
+const TEST_FOLDER = "../dataset/test/";
+
+const classes = ["DRUSEN", "NORMAL"];
+
+const N_CLASSES = 2;
+
+function loadImages(dataDir) {
+  const images = [];
+  const labels = [];
+
+  let dataDir1;
+  let files;
+  let filePath;
+  let buffer;
+  let imageTensor;
+
+  for (let j = 0; j < N_CLASSES; j++) {
+    dataDir1 = dataDir + `/${classes[j]}`;
+    files = fs.readdirSync(dataDir1);
+
+    for (let i = 0; i < 20; i++) {
+      //console.log(`Checking ${classes[j]} images..|| image = ${files[i]}`);
+      if (!files[i].toLocaleLowerCase().endsWith(".jpeg")) {
+        continue;
+      }
+
+      filePath = path.join(dataDir1, files[i]);
+
+      buffer = fs.readFileSync(filePath);
+
+      // imageTensor = tf.node
+      //   .decodeJpeg(buffer, 3)
+      //   .resizeNearestNeighbor([224, 224])
+      //   .toFloat()
+      //   .div(tf.scalar(255.0))
+      //   .expandDims();
+
+      imageTensor = tf.node
+        .decodeJpeg(buffer, 3)
+        .resizeNearestNeighbor((size = [180, 180]))
+        .expandDims();
+
+      images.push(imageTensor);
+      labels.push(j);
+    }
+  }
+  return [images, labels];
+}
+
+class Dataset {
+  constructor() {
+    this.trainData = [];
+    this.testData = [];
+    this.n = N_CLASSES;
+  }
+
+  // Loads training and test data.
+  loadData() {
+    console.log("Loading images...");
+    this.trainData = loadImages(TRAIN_FOLDER);
+    //console.log(this.trainData);
+    this.testData = loadImages(TEST_FOLDER);
+    console.log("Images loaded successfully.");
+  }
+
+  getTrainData() {
+    return {
+      images: tf.concat(this.trainData[0]),
+      labels: tf
+        .oneHot(tf.tensor1d(this.trainData[1], "int32"), this.n)
+        .toFloat(),
+    };
+  }
+  getTestData() {
+    return {
+      images: tf.concat(this.testData[0]),
+      labels: tf
+        .oneHot(tf.tensor1d(this.testData[1], "int32"), this.n)
+        .toFloat(),
+    };
+  }
+}
+
+let dataSet = new Dataset();
+
+async function run(epochs, batchSize, modelSavePath) {
+  dataSet.loadData();
+  const { images: trainImages, labels: trainLabels } = dataSet.getTrainData();
+  //console.log("Training Images (Shape): " + trainImages.shape);
+  //console.log("Training Labels (Shape): " + trainLabels.shape);
+  model.summary();
+
+  const validationSplit = 0.15;
+
+  await model.fit(trainImages, trainLabels, {
+    epochs,
+    batchSize,
+    validationSplit,
+  });
+
+  const { images: testImages, labels: testLabels } = dataSet.getTestData();
+
+  const evalOutput = model.evaluate(testImages, testLabels);
+  console.log(
+    `\nEvaluation result:\n` +
+      `  Loss = ${evalOutput[0].dataSync()[0].toFixed(3)}; ` +
+      `Accuracy = ${evalOutput[1].dataSync()[0].toFixed(3)}`
+  );
+  if (modelSavePath != null) {
+    await model.save(`file://${modelSavePath}`);
+    console.log(`Saved model to path: ${modelSavePath}`);
+  }
+}
+
+async function avaliar() {
+  const modelo = await tf.loadLayersModel(
+    "file://../conversao_01_06/model.json"
+  );
+
+  dataSet.loadData();
+
+  const { images: testImages, labels: testLabels } = dataSet.getTestData();
+
+  modelo.compile({optimizer: "adam",loss: "binaryCrossentropy", metrics: ['accuracy']});
+
+  const evalOutput = modelo.evaluate(testImages, testLabels);
+
+  
+  console.log(
+    `\nEvaluation result:\n` +
+      `  Loss = ${evalOutput[0].dataSync()[0].toFixed(3)}; ` +
+      `Accuracy = ${evalOutput[1].dataSync()[0].toFixed(3)}`
+  );
+}
+avaliar();
+//run(1, 32, "./model");
--- predict.js
+++ predict.js
@@ -1,52 +1,49 @@
-var fs = require('fs');
+const fs = require('fs');
 
 const tf = require('@tensorflow/tfjs-node');
 const ObjectsToCsv = require('objects-to-csv');
-
 
 const folderPath = 'dataset/test/DRUSEN';
 const files = []
 
 fs.readdirSync(folderPath).map(fileName => {
-    files.push({'fileName':fileName,'fullPath':require('path').join(folderPath, fileName)})
+    files.push({ 'fileName': fileName, 'fullPath': require('path').join(folderPath, fileName) })
 })
 
 async function classificar(filePath) {
     const model = await tf.loadLayersModel('file://conversao_01_06/model.json');
 
     const img = fs.readFileSync(filePath);
-    tensor3D = tf.node.decodeJpeg(img,3);
-    tensor3D = tf.image.resizeBilinear(tensor3D, size = [180, 180]); 
+    tensor3D = tf.node.decodeJpeg(img, 3);
+    tensor3D = tf.image.resizeNearestNeighbor(tensor3D, size = [180, 180]).toFloat();
     tensor3D = tensor3D.expandDims();
-    
+
     const prediction = model.predict(tensor3D);
     //console.log(prediction.dataSync()[0]);
 
     return prediction;
 }
 
-let resultData = []
+function predict(){
+    let resultData = []
 
-
-Promise.all(files.map( async (file) => {
+    Promise.all(files.map(async (file) => {
         let result = await classificar(file.fullPath)
-
-        resultData.push({"fileName":file.fileName, "prediction":result.dataSync()[0]})
-    }))
-
-setTimeout( () => {
-    //console.log(resultData)
-
-    (async () => {
+        resultData.push({ "fileName": file.fileName, "prediction": result.dataSync()[0].toString().replace('.', ',') });
+    
+    })).then(async () => {
         const csv = new ObjectsToCsv(resultData);
-       
+    
         // Save to file:
         await csv.toDisk('./drusen_test.csv');
-       
+    
         // Return the CSV file as string:
         console.log(await csv.toString());
-      })();
-}, 2000)
+        
+    })
+}
+
+
 
 
 
