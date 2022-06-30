c569c70698acf3adb429aebdb92035258a86dd9d
Lucas Santana <lucasg4x@gmail.com>
2022-06-30 15:09:13 -0300

    show result of image classification

--- frontend/src/app/file/file-upload/file-upload.component.ts
+++ frontend/src/app/file/file-upload/file-upload.component.ts
@@ -8,6 +8,8 @@ import { finalize } from 'rxjs';
   styleUrls: ['./file-upload.component.scss'],
 })
 export class FileUploadComponent implements OnInit {
+  private api: string = 'http://localhost:3000/upload';
+
   @Input()
   requiredFileType: string = '';
 
@@ -15,44 +17,38 @@ export class FileUploadComponent implements OnInit {
   uploadProgress: any;
   uploadSub: any;
 
+  predict: string = '';
+  resultado: string = '';
+
   constructor(private http: HttpClient) {}
 
   ngOnInit(): void {}
 
   onFileSelected(event: any) {
     const file: File = event.target.files[0];
+    event.target.value = null;
 
     if (file) {
       this.fileName = file.name;
+
       const formData = new FormData();
+
       formData.append('imagem', file);
 
-      const upload$ = this.http
-        .post('http://localhost:3000/upload', formData, {
-          reportProgress: true,
-          observe: 'events',
-        })
-        .pipe(finalize(() => this.reset()));
+      const upload$ = this.http.post(this.api, formData);
 
-      this.uploadSub = upload$.subscribe((event) => {
-        if (event.type == HttpEventType.UploadProgress) {
-          if (event.total) {
-            this.uploadProgress = Math.round(
-              100 * (event.loaded / event.total)
-            );
-          }
-        }
+      upload$.subscribe({
+        next: (retorno: any) => {
+          console.log(
+            `Predict: ${retorno.predict} - Resultado: ${
+              retorno.predict > 0.5 ? 'Normal' : 'Drusen'
+            }`
+          );
+          this.predict = retorno.predict;
+          this.resultado = retorno.predict > 0.5 ? 'Normal' : 'Drusen';
+        },
+        error: (err) => console.log('Error', err),
       });
     }
-  }
-
-  cancelUpload() {
-    this.uploadSub.unsubscribe();
-    this.reset();
-  }
-
-  reset() {
-    this.uploadProgress = null;
-    this.uploadSub = null;
   }
 }
