adb87e2ea6867332b125398b3054b1b1731148ff
Lucas Santana <lucasg4x@gmail.com>
2022-06-30 15:05:38 -0300

    endpoint to upload and classify image

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
-const fileUpload = require('express-fileupload');
+const fileUpload = require("express-fileupload");
+const tf = require("@tensorflow/tfjs-node");
 
 var cors = require("cors");
 const app = express();
@@ -7,7 +8,6 @@ const port = 3000;
 
 app.use(cors());
 app.use(fileUpload());
-
 
 app.get("/", (req, res) => {
   res.send("Ola Mundo");
@@ -17,9 +17,23 @@ app.get("/upload", (req, res) => {
   res.send("Upload arquivo...");
 });
 
-app.post("/upload", (req, res) => {
-  res.send("POST: Upload arquivo...");
-  console.log(req.files.imagem);
+app.post("/upload", async (req, res) => {
+  const model = await tf.loadLayersModel("file://model/conversao_01_11/model.json");
+
+  const img = req.files.imagem.data;
+
+
+  tensor3D = tf.node.decodeJpeg(img, 3);
+
+  tensor3D = tf.image.resizeNearestNeighbor(tensor3D, (size = [180, 180])).toFloat();
+
+  tensor3D = tensor3D.expandDims();
+
+  const prediction = model.predict(tensor3D);
+
+  console.log(`File: ${req.files.imagem.name} Predict: ${prediction.dataSync()[0]}`);
+
+  res.status(200).json({"predict":prediction.dataSync()[0]});
 });
 
 app.listen(port, () => {
