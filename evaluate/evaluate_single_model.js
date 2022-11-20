4d623819e756fc03f939ecc4cf63f63b3e81499a
Lucas Santana <lucasg4x@gmail.com>
2022-11-20 01:25:33 -0300

    Evaluate model in js and converter of multiple keras to json

---
+++ evaluate/evaluate_single_model.js
@@ -0,0 +1,101 @@
+import * as tf from "@tensorflow/tfjs-node";
+import * as fs from "fs";
+import * as path from "path";
+
+export class ModelEvaluate {
+  constructor() {
+    this.dataset = [];
+    this.n = 2;
+    this.classes = ["DRUSEN", "NORMAL"];
+  }
+
+  loadImages(dataDir) {
+    console.log("Loading images...");
+
+    const images = [];
+    const labels = [];
+
+    let dataDir1;
+    let files;
+    let filePath;
+    let buffer;
+    let imageTensor;
+
+    let NORMAL_DRUSEN_IMAGES = [];
+
+    for (let j = 0; j < this.n; j++) {
+      dataDir1 = dataDir + `/${this.classes[j]}`;
+      files = fs.readdirSync(dataDir1);
+      let count = 0;
+      for (let i = 0; i < files.length; i++) {
+        if (!files[i].toLocaleLowerCase().endsWith(".jpeg")) {
+          continue;
+        }
+
+        filePath = path.join(dataDir1, files[i]);
+
+        //console.log(`Checking ${this.classes[j]} images..|| image = ${filePath}`);
+
+        buffer = fs.readFileSync(filePath);
+
+        // tidy limpa todos os tf.Tensor s que não são retornados por uma função depois de executá-la
+        // apenas o tensor retornado por expandDims() será mantido na memória
+        imageTensor = tf.tidy(() => {
+          return tf.node
+            .decodeJpeg(buffer, 3)
+            .resizeNearestNeighbor([180,180])
+            .expandDims();
+        });
+
+        //console.log("numTensors: " + tf.memory().numTensors);
+
+        images.push(imageTensor);
+
+        labels.push(j);
+
+        count++;
+      }
+      NORMAL_DRUSEN_IMAGES[j] = count;
+    }
+
+    console.log("IMAGENS DRUSEN: " + NORMAL_DRUSEN_IMAGES[0]);
+    console.log("IMAGENS NORMAL: " + NORMAL_DRUSEN_IMAGES[1]);
+
+    console.log("Images loaded successfully.");
+    return [images, labels];
+  }
+
+  async getDataset(folder) {
+    this.dataset = this.loadImages(folder);
+    this.dataset = {
+      images: tf.concat(this.dataset[0]),
+      labels: tf.tensor1d(this.dataset[1], "int32"),
+    };
+  }
+
+  async evaluate(modelPath) {
+    const { images: images, labels: labels } = this.dataset;
+
+    const model = await tf.loadLayersModel(modelPath);
+
+    const optimizer = tf.train.adam();
+
+    model.compile({
+      optimizer: optimizer,
+      loss: "binaryCrossentropy",
+      metrics: ["accuracy"],
+    });
+
+    const evalOutput = model.evaluate(images, labels, {
+      batchSize: 8,
+    });
+
+    console.log(
+      `\nEvaluation result:\n` +
+        `  Loss = ${evalOutput[0].dataSync()[0].toFixed(3)}; ` +
+        `Accuracy = ${evalOutput[1].dataSync()[0].toFixed(3)}`
+    );
+
+    return modelPath;
+  }
+}
---
+++ evaluate/index.js
@@ -0,0 +1,29 @@
+import { ModelEvaluate } from "./evaluate_single_model.js";
+import * as fs from "fs";
+
+let model = new ModelEvaluate();
+
+let dirs = fs.readdirSync("../conversao/json/");
+
+if (!dirs.length > 0) {
+  throw "pasta conversao/json/vazia...";
+}
+
+(async function () {
+  await model.getDataset("../dataset/less");
+
+  for (const dir of dirs) {
+    let path = "../conversao/json/" + dir;
+    let files = fs.readdirSync(path);
+    let modelPath = "file://" + path + "/" + files[1];
+
+    console.log("Modelo: " + modelPath + "\n");
+    const teste = await model.evaluate(modelPath);
+    console.log("Avaliado: " + teste + "\n");
+  }
+})();
+
+// let modelPath = "file://../conversao/20221107_041534/model.json";
+// let model = new ModelEvaluate();
+// model.getDataset("../dataset/less");
+// model.evaluate(modelPath);
